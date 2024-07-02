f5f6212a966ff510e8cafdcb7db0b01d31e0cad5
mimizinha27 <00001107826226sp@al.educacao.sp.gov.br>
2024-07-02 16:44:28 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(600, 600);
+    background('black')
+  }
+  
+  function draw() {
+    stroke("blue");
+    fill('red');
+
+ if(mouseIsPressed){
+ Rect(mouseX, mouseY, 20,35);
+ }
+
+ {
